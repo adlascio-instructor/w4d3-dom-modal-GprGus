50bfd53547501fd48bc91cb260f78b5fcd521bed
Gustavo <gpr.gus@icloud.com>
2023-03-01 23:58:50 -0800

    .

--- modal.js
+++ modal.js
@@ -1,9 +1,24 @@
 console.log("here");
 
+
+
 // select button element
+
+const openModal = document.getElementById("openModal");
+const modal = document.getElementById("modal");
 
 // add event listener on click to button
 // remove class invisible from div modal
 
+openModal.addEventListener("click", () => {
+    modal.classList.remove("invisible");
+});
+
 // add event listener on key press Escape
 // add class invisible to div modal
+
+document.addEventListener("keydown", (event) => {
+    if (event.key == "Escape") {
+        modal.classList.add("invisible");
+    }
+});
